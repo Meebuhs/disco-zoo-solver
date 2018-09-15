c62add08c959ced447ebb21c1aa84a6aa5496f2d
Meebuhs <alexeksharpe@gmail.com>
2018-09-15 18:48:51 +1000

    Change animal selection dropdowns to toggle buttons

--- src/main/java/ui/ToolPane.java
+++ src/main/java/ui/ToolPane.java
@@ -7,7 +7,10 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Separator;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 import java.util.ArrayList;
@@ -16,131 +19,144 @@ import java.util.List;
 class ToolPane {
     private SolverApp solver;
     private ToolBar toolBar;
-    private List<ComboBox<String>> dropdowns;
+    private ComboBox<String> locationDropdown;
+    private List<ToggleButton> animalButtons;
+    private int numberSelected;
+    private List<String> selectedAnimals;
+    private Button startButton;
 
     ToolPane(SolverApp solver) {
         this.solver = solver;
-        this.dropdowns = new ArrayList<>();
+        this.locationDropdown = createLocationDropdown();
+        this.startButton = createStartButton();
+        this.animalButtons = new ArrayList<>();
+        this.selectedAnimals = new ArrayList<>();
+        this.numberSelected = 0;
+        initialiseAnimalButtons(locationDropdown.getSelectionModel().getSelectedItem());
         this.toolBar = new ToolBar(createAnimalSelection(), new Separator());
         this.toolBar.setPrefHeight(40);
-        initiateDropdowns();
+    }
+
+    private ComboBox<String> createLocationDropdown() {
+        ObservableList<String> locationOptions = FXCollections.observableArrayList(solver.getLocationList());
+
+        ComboBox<String> locationDropdown = new ComboBox<>();
+
+        locationDropdown.setPrefHeight(50);
+        locationDropdown.setPrefWidth(150);
+
+        locationDropdown.setItems(locationOptions);
+        locationDropdown.getSelectionModel().selectFirst();
+
+        locationDropdown.setOnAction((ActionEvent event) ->
+                updateAnimalOptions(locationDropdown.getSelectionModel().getSelectedItem()));
+
+        return locationDropdown;
+    }
+
+    private Button createStartButton() {
+        Button startButton = new Button();
+        startButton.setPrefHeight(50);
+        startButton.setText("Start Game");
+        startButton.setDisable(true);
+        startButton.setOnAction((ActionEvent event) -> startGame());
+        return startButton;
+    }
+
+    private void initialiseAnimalButtons(String location) {
+        for (int i = 0; i < 6; i++) {
+            animalButtons.add(createToggleButton());
+        }
+        updateAnimalOptions(location);
+    }
+
+    private ToggleButton createToggleButton() {
+        ToggleButton button = new ToggleButton();
+        button.setOnAction(this::handleAnimalSelect);
+        return button;
+    }
+
+    private void updateAnimalOptions(String location) {
+        List<String> animals = solver.getAnimalsFromLocation(location);
+        for (int i = 0; i < animalButtons.size(); i++) {
+            ToggleButton button = animalButtons.get(i);
+            String path = String.format("animals/%s.png", animals.get(i));
+            ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+            Image image = new Image(classLoader.getResourceAsStream(path));
+            ImageView imageView = new ImageView(image);
+            imageView.setFitWidth(45);
+            imageView.setPreserveRatio(true);
+            imageView.setSmooth(true);
+            imageView.setCache(true);
+            button.setGraphic(imageView);
+            button.setPrefSize(45, 45);
+        }
+        resetSelectedButtons();
+    }
+
+    private void resetSelectedButtons() {
+        for (ToggleButton button : animalButtons) {
+            button.setSelected(false);
+            button.setDisable(false);
+        }
+        numberSelected = 0;
+        selectedAnimals.clear();
     }
 
     private HBox createAnimalSelection() {
-        createDropdowns(4);
-
-        Button startGame = new Button();
-        startGame.setText("Start Game");
-        startGame.setOnAction((ActionEvent event) -> startGame());
-
         HBox animalSelection = new HBox();
-        animalSelection.getChildren().addAll(dropdowns);
-        animalSelection.getChildren().add(startGame);
-        animalSelection.setSpacing(10);
+        animalSelection.getChildren().add(locationDropdown);
+        animalSelection.getChildren().addAll(animalButtons);
+        animalSelection.getChildren().add(startButton);
+        animalSelection.setSpacing(20);
         animalSelection.setAlignment(Pos.CENTER_LEFT);
 
         return animalSelection;
     }
 
-    private void initiateDropdowns() {
-        ObservableList<String> locationOptions = FXCollections.observableArrayList(solver.getLocationList());
-
-        ComboBox<String> locationDropdown = dropdowns.get(0);
-        locationDropdown.setItems(locationOptions);
-        locationDropdown.getSelectionModel().selectFirst();
-
-        initialiseAnimalOptions(locationDropdown.getSelectionModel().getSelectedItem());
-    }
-
-    private void initialiseAnimalOptions(String location) {
-        for (int i = 1; i < dropdowns.size(); i++) {
-            ComboBox<String> animalDropdown = dropdowns.get(i);
-            if (i == 1) {
-                animalDropdown.setItems(FXCollections.observableArrayList(
-                        solver.getAnimalsFromLocation(location)));
-            } else {
-                ObservableList<String> animalOptionsWithNone = FXCollections.observableArrayList(
-                        solver.getAnimalsFromLocation(location));
-                animalOptionsWithNone.add(0, "None");
-                animalDropdown.setItems(animalOptionsWithNone);
-            }
+    private void handleAnimalSelect(ActionEvent event) {
+        ToggleButton clickedButton = (ToggleButton) event.getSource();
+        int clickedIndex = animalButtons.indexOf(clickedButton);
+        String clickedAnimal = solver.getAnimalsFromLocation(
+                locationDropdown.getSelectionModel().getSelectedItem()).get(clickedIndex);
+        if (clickedButton.isSelected()) {
+            numberSelected++;
+            selectedAnimals.add(clickedAnimal);
+            setButtonStates(true);
+        } else {
+            numberSelected--;
+            selectedAnimals.remove(clickedAnimal);
+            setButtonStates(false);
         }
     }
 
-    private void createDropdowns(int number) {
-        for (int i = 0; i < number; i++) {
-            dropdowns.add(createDropdown(100));
-        }
-
-        setDropdownOnActions();
-    }
-
-    private ComboBox<String> createDropdown(int width) {
-        ComboBox<String> comboBox = new ComboBox<>();
-        comboBox.setPrefWidth(width);
-        return comboBox;
-    }
-
-    private void setDropdownOnActions() {
-        ComboBox<String> locationDropdown = dropdowns.get(0);
-        locationDropdown.setOnAction((ActionEvent event) ->
-                initialiseAnimalOptions(locationDropdown.getSelectionModel().getSelectedItem()));
-        for (int i = 1; i < dropdowns.size(); i++) {
-            ComboBox<String> animalDropdown = dropdowns.get(i);
-            animalDropdown.valueProperty().addListener((obs, oldAnimal, newAnimal) -> updateAvailableAnimalOptions
-                    (dropdowns.indexOf(animalDropdown), oldAnimal, newAnimal));
-        }
-    }
-
-    private void updateAvailableAnimalOptions(int dropdownIndex, String oldAnimal, String newAnimal) {
-        for (int i = 1; i < dropdowns.size(); i++) {
-            ComboBox<String> animalDropdown = dropdowns.get(i);
-            if (!(i == dropdownIndex)) {
-                if (!(newAnimal == null || newAnimal.equals("None"))) {
-                    animalDropdown.getItems().remove(newAnimal);
+    private void setButtonStates(boolean animalAdded) {
+        if (animalAdded) {
+            if (numberSelected == 3) {
+                for (ToggleButton button : animalButtons) {
+                    if (!(button.isSelected())) {
+                        button.setDisable(true);
+                    }
                 }
-                if (!(oldAnimal == null || oldAnimal.equals("None"))) {
-                    animalDropdown.getItems().add(getAnimalIndex(oldAnimal, i), oldAnimal);
+            } else if (numberSelected == 1) {
+                startButton.setDisable(false);
+            }
+        } else {
+            if (numberSelected == 2) {
+                for (ToggleButton button : animalButtons) {
+                    if (button.isDisable()) {
+                        button.setDisable(false);
+                    }
                 }
+            } else if (numberSelected == 0) {
+                startButton.setDisable(true);
             }
         }
-    }
-
-    private int getAnimalIndex(String oldAnimal, int dropdownIndex) {
-        ComboBox<String> dropdown = dropdowns.get(dropdownIndex);
-        List<String> animals = solver.getAnimalsFromLocation(dropdowns.get(0).getSelectionModel().getSelectedItem());
-
-        int index = 0;
-        if (dropdownIndex > 1) {
-            if (oldAnimal.equals("None")) {
-                return 0;
-            } else {
-                index++;
-            }
-        }
-
-        for (int i = index; i < dropdown.getItems().size(); i++) {
-            if (animals.indexOf(oldAnimal) > animals.indexOf(dropdown.getItems().get(i))) {
-                index++;
-            }
-        }
-
-        return index;
     }
 
     private void startGame() {
-        List<String> animals = new ArrayList<>();
-        ComboBox<String> locationDropdown = dropdowns.get(0);
-        String location = locationDropdown.getValue();
-        for (int i = 1; i <= 3; i++) {
-            ComboBox<String> animalDropdown = dropdowns.get(i);
-            String selected = animalDropdown.getSelectionModel().getSelectedItem();
-            if (!(selected == null || selected.equals("None"))) {
-                animals.add(selected);
-            }
-        }
-        if (!(animals.isEmpty())) {
-            solver.startGame(location, animals);
+        if (!(selectedAnimals.isEmpty())) {
+            solver.startGame(locationDropdown.getSelectionModel().getSelectedItem(), selectedAnimals);
         }
     }
 
